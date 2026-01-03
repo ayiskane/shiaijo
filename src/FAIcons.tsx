f470d916613eddce3e0a9892d46d21c9c8018fef
ayiskane <iksreceipts@outlook.com>
2026-01-02 23:54:04 -0800

    Replace CheckToSlot with MessageCheck icon

--- src/FAIcons.tsx
+++ src/FAIcons.tsx
@@ -228,9 +228,9 @@ export const AlertTriangle = ({ className, ...props }: IconProps) => (
   </svg>
 )
 
-export const CheckToSlot = ({ className, ...props }: IconProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className={className} {...props}>
-    <path opacity=".4" fill="currentColor" d="M0 128C0 92.7 28.7 64 64 64l448 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128zm128 32a32 32 0 1 0 0 64 32 32 0 1 0 0-64zm32 128a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 352a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/>
-    <path fill="currentColor" d="M527.4 164.7c6.2 6.2 6.2 16.4 0 22.6l-176 176c-6.2 6.2-16.4 6.2-22.6 0l-80-80c-6.2-6.2-6.2-16.4 0-22.6s16.4-6.2 22.6 0L340 329.4 504.7 164.7c6.2-6.2 16.4-6.2 22.6 0z"/>
+export const MessageCheck = ({ className, ...props }: IconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className={className} {...props}>
+    <path opacity=".4" fill="currentColor" d="M0 64C0 28.7 28.7 0 64 0L448 0c35.3 0 64 28.7 64 64l0 288c0 35.3-28.7 64-64 64l-138.7 0L185.6 508.8c-4.8 3.6-11.3 4.2-16.8 1.5s-8.8-8.2-8.8-14.3l0-80-96 0c-35.3 0-64-28.7-64-64L0 64z"/>
+    <path fill="currentColor" d="M369 175c9.4 9.4 9.4 24.6 0 33.9L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0z"/>
   </svg>
 )
