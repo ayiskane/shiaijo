6467976f5bf15fa0d62dda95b73107aa6f973527
ayiskane <iksreceipts@outlook.com>
2026-01-11 20:20:06 -0800

    Add admin components index

---
+++ src/lib/components/admin/index.ts
@@ -0,0 +1,4 @@
+export { default as AdminSidePanel } from './AdminSidePanel.svelte';
+export { default as AdminTopBar } from './AdminTopBar.svelte';
+export { default as AdminToolbar } from './AdminToolbar.svelte';
+export { default as AdminDataTable } from './AdminDataTable.svelte';
